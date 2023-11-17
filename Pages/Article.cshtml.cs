c44e4e556001fbd20175b5bbd49efa8e81487909
3BoOoD97 <abodn70@hotmail.com>
2023-11-17 16:17:31 +0100

    Designing Publishers and Articles pages

---
+++ Pages/Article.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArticalProject.Pages
+{
+    public class ArticleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
